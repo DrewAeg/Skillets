72d06f08b09a363b29e75e8a005a6fbd82937e58
Nathan <nembery@gmail.com>
2019-04-08 15:35:50 -0400

    update azure example

--- cloud/azure/single_pavm/variables.tf
+++ cloud/azure/single_pavm/variables.tf
@@ -1,22 +1,22 @@
-variable "subscription_id" {
-  description = "Azure Subscription Id"
-  default = "0000-0000-0000-0000"
-}
-
-variable "client_id" {
-  description = "Client Id"
-  default = "0000-0000-0000-0000"
-}
-
-variable "client_secret" {
-  description = "Client Secret"
-  default = "0000-0000-0000-0000"
-}
-
-variable "tenant_id" {
-  description = "Tenant Id"
-  default = "0000-0000-0000-0000"
-}
+//variable "subscription_id" {
+//  description = "Azure Subscription Id"
+//  default = "0000-0000-0000-0000"
+//}
+//
+//variable "client_id" {
+//  description = "Client Id"
+//  default = "0000-0000-0000-0000"
+//}
+//
+//variable "client_secret" {
+//  description = "Client Secret"
+//  default = "0000-0000-0000-0000"
+//}
+//
+//variable "tenant_id" {
+//  description = "Tenant Id"
+//  default = "0000-0000-0000-0000"
+//}
 
 variable "admin_username" {
   description = "PAN-OS NGFW Admin Username"
