f8938da7de02705fb2e562f378ca3411fb2da4be
Nathan <nembery@gmail.com>
2019-04-09 15:17:29 -0400

    use data to pull out public ip

--- cloud/azure/single_pavm/outputs.tf
+++ cloud/azure/single_pavm/outputs.tf
@@ -1,7 +1,8 @@
-output "pavm_public_ip_address" {
-    value = "${azurerm_public_ip.pavm_public_ip.ip_address}"
+data "azurerm_public_ip" "pavm_public_ip_address_data" {
+  name                = "${azurerm_public_ip.pavm_public_ip.name}"
+  resource_group_name = "${azurerm_virtual_machine.pavm.resource_group_name}"
 }
 
-output "pavm_resource_group" {
-    value = "${azurerm_resource_group.panhandler.name}"
+output "pavm_public_ip_address" {
+  value = "${data.azurerm_public_ip.pavm_public_ip_address_data.ip_address}"
 }
