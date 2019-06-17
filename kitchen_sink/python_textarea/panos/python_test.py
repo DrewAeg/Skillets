5b0bac502c51377214d52738e5c9c0c33a8d1754
Nathan <nembery@gmail.com>
2019-06-17 15:42:48 -0400

    update python test

--- kitchen_sink/python_textarea/panos/python_test.py
+++ kitchen_sink/python_textarea/panos/python_test.py
@@ -1,2 +1,10 @@
-<hostname>{{ FW_NAME }}</hostname>
-<more>{{ test_var }}</more>
+#!/usr/bin/env python3
+
+import sys
+print('*'*80)
+
+for i in sys.argv:
+    print('Found argument:')
+    print(i)
+
+print('*'*80)
